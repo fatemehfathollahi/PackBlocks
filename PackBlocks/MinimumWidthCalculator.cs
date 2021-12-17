e77451459bcd4d9416674630dc0e37d50b074f01
f.fathollahi <ff.fathollahi@gmail.com>
2021-12-17 11:24:25 +0330

    Add project files.

---
+++ PackBlocks/MinimumWidthCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackBlocks
+{
+    public class MinimumWidthCalculator
+    {
+        public int PackBlock(int[] blocks, int height)
+        {
+            int start = blocks.Max();
+            int end = 100000;
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                int subSum = 0;
+                int numberOfSubArray = 1;
+                for (int i = 0; i < blocks.Length; i++)
+                {
+                    if (subSum + blocks[i] > mid)
+                    {
+                        subSum = blocks[i];
+                        numberOfSubArray++;
+                    }
+                    else
+                    {
+                        subSum += blocks[i];
+                    }
+                }
+
+                if (numberOfSubArray <= height)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+
+            return start;
+        }
+    }
+}
---
+++ PackBlocks/MinimumWidthCalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PackBlocks
+{
+    public class MinimumWidthCalculatorTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3, 1, 3, 3 }, 2, 6)]
+        [InlineData(new int[] { 2, 3, 1, 1, 1 }, 2, 5)]
+        [InlineData(new int[] { 2, 3, 1, 1 }, 2, 5)]
+        [InlineData(new int[] { 2, 3, 1, 1 }, 3, 3)]
+        [InlineData(new int[] { 2, 3, 1, 1 }, 4, 3)]
+        [InlineData(new int[] { 2, 3, 1, 1, 1, 1 }, 4, 3)]
+        public void PackBlocksTest(int[] blocks, int height, int assertedMinimumWidth)
+        {
+            var calculator = new MinimumWidthCalculator();
+
+            var calculatedMinimumWidth = calculator.PackBlock(blocks, height);
+
+            calculatedMinimumWidth.Should().Be(assertedMinimumWidth);
+        }
+    }
+}
